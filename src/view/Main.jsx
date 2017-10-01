0d07953b5cd962699eb5adf702ed2e9eca2030a0
KizyLyrio <kizylirio@gmail.com>
2017-10-01 15:18:25 -0300

    create menu

---
+++ src/view/Main.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Menu from './Menu'
+// import './Menu.css';
+
+class Main extends Component {
+    render() {
+      return (
+        <Menu/>
+      );
+    }
+  }
+  
+  export default Main;
