348386ea4009e33c8eb216222739374e78e80d3d
KizyLyrio <kizylirio@gmail.com>
2017-10-01 22:28:20 -0300

    Add Button

--- src/view/Main.jsx
+++ src/view/Main.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Menu from './Menu'
 import Card from './Card'
 import Session from './Session'
+import { Button } from 'react-bootstrap';
 
 class Main extends Component {
     render() {
@@ -9,7 +10,9 @@ class Main extends Component {
         <div>
           <Menu/>
           <Card/>
+          <Button bsSize="small" bsStyle="success">Gravar</Button>
           <Session />
+          
         </div>
       );
     }
