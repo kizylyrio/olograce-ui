8a0e846a93e1038b0b04e4a60b6ad12874f0c8ad
KizyLyrio <kizylirio@gmail.com>
2017-10-02 10:49:04 -0300

    add team list

--- src/view/RegisterForm.jsx
+++ src/view/RegisterForm.jsx
@@ -9,10 +9,18 @@ class RegisterForm extends Component {
           <ControlLabel>Nome</ControlLabel>
           <FormControl type="text"/>
         </FormGroup>
-        <FormGroup>
-          <ControlLabel>Equipe</ControlLabel>
-          <FormControl type="text"/>
-        </FormGroup>
+        <FormGroup controlId="formControlsSelect">
+        <ControlLabel>Equipe</ControlLabel>
+        <FormControl componentClass="select" placeholder="select">
+          <option value="dores">Dores</option>
+          <option value="fatima">Fátima</option>
+          <option value="gracas">Graças</option>
+          <option value="imaculada">Imaculada Conceição</option>
+          <option value="lourdes">Loudes</option>
+          <option value="salete">Salete</option>
+          <option value="silencio">Silêncio</option>
+        </FormControl>
+      </FormGroup>
         <Button type="submit" bsSize="small" bsStyle="success">Gravar</Button>
       </form>
       );
